5263bcd0bd9e13f7a6d2e469ae2dcc41bacce7cc
Jaymin Modi <Jaymin.Modi@ignitiongroup.co.za>
2023-08-04 18:58:12 +0530

    Addedchanges regards to prepare start logs

--- DaprMicroserviceTemplate/Middleware/ExceptionMiddleware.cs
+++ DaprMicroserviceTemplate/Middleware/ExceptionMiddleware.cs
@@ -1,10 +1,11 @@
-﻿using log4net;
+﻿using AutoWrapper.Wrappers;
+using log4net;
 using Microsoft.AspNetCore.Http;
+using System;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using System;
-using AutoWrapper.Wrappers;
+//using LogzLoggerProvider;
 
 namespace DaprMicroserviceTemplate.Middleware
 {
@@ -22,7 +23,7 @@ namespace DaprMicroserviceTemplate.Middleware
             var request = httpContext?.Request;
             try
             {
-                if (request.Method == HttpMethods.Post && request.ContentLength > 0)
+                if ((request.Method == HttpMethods.Post || request.Method == HttpMethods.Put) && request.ContentLength > 0)
                 {
                     request.EnableBuffering();
                     var buffer = new byte[Convert.ToInt32(request.ContentLength)];
@@ -31,26 +32,27 @@ namespace DaprMicroserviceTemplate.Middleware
                     requestContent = Encoding.UTF8.GetString(buffer);
                     request.Body.Position = 0;  //rewinding the stream to 0
                 }
+                var requestedData = PrepareRequestedData(httpContext, requestContent);
+
+                //Please uncomment below line and allow for global start logs
+                //   _logger.ServiceStarted(list: requestData);
+
                 await _next(httpContext);
             }
             catch (Exception ex)
             {
-                var requestData = new object[]{
-                request?.Method,
-                requestContent,
-                request?.Query};
-
-                await HandleExceptionAsync(httpContext, ex, requestData);
+                var requestedData = PrepareRequestedData(httpContext, requestContent);
+                await HandleExceptionAsync(httpContext, ex, requestedData);
             }
         }
-        private async Task HandleExceptionAsync(HttpContext context, Exception exception, object[] requestBody)
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception, object[] requestedData)
         {
             var path = context.Request?.Path;
             int statusCode;
-            
-            //Please remove below Commented line and allow for global exception logs
-            //_logger.ServiceFaulted(exception, path, requestBody);
-            
+
+            //Please uncomment below line and allow for global exception logs
+            //_logger.ServiceFaulted(exception, path, requestedData);
+
             switch (true)
             {
                 case bool _ when exception is ApiException:
@@ -64,6 +66,16 @@ namespace DaprMicroserviceTemplate.Middleware
             context.Response.StatusCode = statusCode;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(exception.GetBaseException().Message);
+        }
+
+        private object[] PrepareRequestedData(HttpContext context, string requestBodyJson)
+        {
+            var request = context?.Request;
+            var requestData = new object[]{
+                request?.Method,
+                requestBodyJson,
+                request?.QueryString};
+            return requestData;
         }
 
     }
